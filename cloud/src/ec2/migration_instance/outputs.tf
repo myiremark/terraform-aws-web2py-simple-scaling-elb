913c885f211899ab94ede41f20be42c7b0ddf2e4
Mark Graves <mark@myire.com>
2020-05-29 03:02:48 -0700

    convenience output

--- cloud/src/ec2/migration_instance/outputs.tf
+++ cloud/src/ec2/migration_instance/outputs.tf
@@ -2,3 +2,8 @@ output "migration_instance_public_ip" {
   # we need this for easy testing since we're not using DNS
   value = aws_instance.webappmigrationinstance.public_ip
 }
+
+output "migration_instance_test_endpoint" {
+  value = "${aws_instance.webappmigrationinstance.public_ip}${var.app_ready_check_path}"
+}
+
